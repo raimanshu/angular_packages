a6db5cf6d80c6029a7208d41f103d3f30ab66871
Himanshu Rai <rai.official9@gmail.com>
2021-02-12 15:22:01 +0530

    Integrated map in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBBwhKViA9NfSWCPess_CU5kRnvw2HWYTQ'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
